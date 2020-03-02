0195dfdfdc4f2ecd4cbd8dc77ef3f65d3068b304
Li <617612567@qq.com>
2020-03-02 19:04:44 +0800

    完成布局,但内容都不完整

--- sys/src/views/Welcome.vue
+++ sys/src/views/Welcome.vue
@@ -1,24 +1,71 @@
 <template>
 <div>
-    <!-- <el-row :gutter="10">
-        <el-col :xs="8" :sm="8" :md="8" :lg="8" :xl="8">
+    <el-container>
+        <!-- <el-row>
+            <el-col :span="5"> -->
+        <!--sidebar要设置自适应,全展示=>仅剩图标=>全消失  -->
+
+        <el-aside width="200px">
             <sidebar />
-        </el-col>
-        <el-col :xs="16" :sm="16" :md="16" :lg="16" :xl="16">
-            <navbar />
-            <app-main />
-        </el-col>
-    </el-row>
-    <sidebar /> -->
+        </el-aside>
+        <!-- </el-col>
+            <el-col :span="19"> -->
+        <!-- <navbar />
+            <app-main /> -->
+        <el-container>
+            <el-header>
+                <navi />
+            </el-header>
+            <el-main>
+                <router-view />
+            </el-main>
+        </el-container>
+
+        <!-- </el-col>
+        </el-row> -->
+    </el-container>
 </div>
 </template>
 
 <script>
+import sidebar from '../components/layout/sidebar'
+import navi from '../components/layout/nav'
 export default {
-
+    components: {
+        sidebar,
+        navi
+    }
 }
 </script>
 
 <style scoped>
+div,
+.el-container {
+    height: 100%;
+}
+/* 
+.el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+} */
 
+/* .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+} */
+
+.el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+}
+
+body>.el-container {
+    margin-bottom: 40px;
+}
 </style>
